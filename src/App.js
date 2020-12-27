d32b7b6e0b0d4134ec575319dc9a090e1fd5a4ef
Leticia Costa Fernandes <leticiacf.fernandes@gmail.com>
2020-12-27 14:36:39 -0300

    feat(fase 1): criar boilerplate do projeto e criar componente App com lista mockada de afazeres

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import './App.css';
 function App() {
   return (
     <div>
-      Hello world
+      <input type="checkbox" />
+      <p>Texto para a checkbox</p>
+
+      <input type="checkbox" />
+      <p>Texto para a checkbox 2</p>
+      
+      <input type="checkbox" />
+      <p>Texto para a checkbox 3</p>
     </div>
   );
 }
